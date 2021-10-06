e1b2c0909b0066cb3da8333457c85b81fefa84bd
nitansha <72187268+nitansha@users.noreply.github.com>
2021-10-06 19:54:56 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// void file_i_o()
+// {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     #ifndef ONLINE_JUDGE
+//     freeopen("input.txt","r",stdin);
+//     freeopen("output.txt","w",stdout);
+    
+//     #endif
+// }
+
+
+int main() {
+    
+  //  file_i_o();
+    
+    // long t;
+    
+   // cin>>t;
+  //  t = 1;
+    // while(t--)
+    // {
+   
+    
+  int a[4]= {1,4,5,3};
+   
+  std:: sort(a,a+4);
+    
+     if(a[0]==Ba[3]){
+      cout<<"0"<<endl;
+    }
+    else if(a[0]==a[2] || a[1]==a[3]){
+      cout<<"1"<<endl;
+    }
+    else{
+      cout<<"2"<<endl;
+        }
+   
+	return 0;
+}
